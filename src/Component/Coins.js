7ad290396ec600ab72d266eb1c71c7c0edbcfccc
dongmin30 <dongmin0355@gmail.com>
2024-06-03 13:39:28 +0900

    Coin Tracker 코드 챌린지 완

--- src/Component/Coins.js
+++ src/Component/Coins.js
@@ -1,21 +1,33 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export function Coins() {
   const [loading, setLoading] = useState(true);
   // 기본 값을 지정 후 사용해야한다. 지정하지 않을 시
   // coins의 초기값이 undefind가 되어 coins.length에서 오류가 발생하기 때문이다.
   const [coins, setCoins] = useState([]);
-  const [myMoney, setMyMoney] = useState(20);
+  const [coinPrice, setCoinPrice] = useState(1);
+  const [myMoney, setMyMoney] = useState(500);
+  const moneyInputRef = useRef(null);
+
+  const coinOnChange = (event) => {
+    setCoinPrice(event.target.value);
+  }
+
   const onSubmit = (event) => {
     event.preventDefault();
-    console.log(event);
+    const newMoney = moneyInputRef.current.value;
+    if (newMoney) {
+      setMyMoney(Number(newMoney));
+    }
   }
+
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
     .then(response => response.json())
     .then((json) => {
-      setCoins(json);
+      setCoins(json);      
       setLoading(false);
+      setCoinPrice(json[0].quotes.USD.price);
     });
   }, []);
 
@@ -25,21 +37,26 @@ export function Coins() {
       <h2>You have {myMoney}$</h2>
       <form onSubmit={onSubmit}>
         <input 
-          type="text" 
-          name="myMoney" 
+          type="text"
           placeholder="Input your money." 
+          ref={moneyInputRef}
         />
         <button>Add my money</button>
       </form>
-      <p>You can buy this coin </p>
-      {loading ? <strong>Loading...</strong> : null}
-      <select>
-        {coins.map((coin) => (
-          <option>
-            {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price}
-          </option>
-        ))}
-      </select>
+      {loading ? (
+        <strong>Loading...</strong>
+        ) : (
+          <div>
+            <p>You can buy this coin {myMoney / coinPrice}</p>
+            <select onChange={coinOnChange}>
+              {coins.map((coin) => (
+                <option key={coin.id} value={coin.quotes.USD.price}>
+                  {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price}
+                </option>
+              ))}
+            </select>
+          </div>
+        )}
     </div>
   )
 }
